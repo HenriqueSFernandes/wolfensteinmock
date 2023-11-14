7f90163d733c7c44af4f66778ee38564a8830ce4
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2023-11-14 11:33:09 +0000

    Now the color depends on the distance to the wall.
    The closer you are to the wall the brighter it is.

--- src/main/java/org/entities/Player.java
+++ src/main/java/org/entities/Player.java
@@ -57,18 +57,23 @@ public class Player {
     }
 
     private void rayCaster(TextGraphics graphics) {
-        // TODO change the algorithm to a DDA algorithm.
-        graphics.setBackgroundColor(TextColor.Factory.fromString("#0000FF"));
+        // TODO change the algorithm to a DDA algorithm if it's better.
+        // TODO fix fisheye effect.
         for (int x = 0; x < Window.WIDTH; x++) {
             double rayAngle = angle - 35 + (70.0 * x / Window.WIDTH);
             List<Position> line = createLine(rayAngle);
-//            Raycaster Render
+
+            // Raycaster Render
             for (Position point : line) {
                 graphics.setCharacter(point.getX(), point.getY(), ' ');
             }
             Position collisionPoint = line.get(line.size() - 1);
-            int distanceToWall = (int) (Math.sqrt(Math.pow(collisionPoint.getX() - position.getX(), 2) + Math.pow(collisionPoint.getY() - position.getY(), 2)));
-            int wallHeight = distanceToWall != 0 ? (int) ((Window.HEIGHT * Window.CELLSIZE) / distanceToWall) : Window.HEIGHT;
+            double distanceToWall = Math.sqrt(Math.pow(collisionPoint.getX() - position.getX(), 2) + Math.pow(collisionPoint.getY() - position.getY(), 2));
+            distanceToWall *= Math.abs(Math.cos(Math.toRadians(rayAngle - angle))); // TODO Fisheye correction not working when the angle is 90 or 270.
+            int color = mapColor(distanceToWall);
+            graphics.setBackgroundColor(TextColor.Factory.fromString("#0000" + String.format("%02X", color)));
+            int maxWallHeight = Window.HEIGHT;
+            int wallHeight = (int) ((Window.HEIGHT * Window.CELLSIZE) / distanceToWall);
             int drawStart = -wallHeight / 2 + Window.HEIGHT / 2;
             if (drawStart < 0) drawStart = 0;
             int drawEnd = wallHeight / 2 + Window.HEIGHT / 2;
@@ -79,7 +84,6 @@ public class Player {
             int wallScreenX = (int) (Window.WIDTH * (0.5 + wallX / 2.0));
 
             graphics.drawLine(2 * Window.WIDTH - wallScreenX, drawStart, 2 * Window.WIDTH - wallScreenX, drawEnd, ' ');
-
         }
     }
 
@@ -92,13 +96,15 @@ public class Player {
         List<Position> line = new ArrayList<Position>();
         int x1 = position.getX();
         int y1 = position.getY();
-        int distance = 1000; // You may need to adjust this based on your requirements
+        int distance = 1000;
         int x2 = (int) (x1 + distance * Math.cos(Math.toRadians(angle)));
         int y2 = (int) -(y1 + distance * Math.sin(Math.toRadians(angle)));
         int dx = Math.abs(x2 - x1);
         int dy = Math.abs(y2 - y1);
         int sx = (x1 < x2) ? 1 : -1;
         int sy = (y1 < y2) ? 1 : -1;
+
+        int side = 1; // 1 for top/bottom, 2 for left/right
 
         int err = dx - dy;
 
@@ -112,13 +118,21 @@ public class Player {
             if (e2 > -dy) {
                 err -= dy;
                 x1 += sx;
+                side = 2;
+
             }
             if (e2 < dx) {
                 err += dx;
                 y1 += sy;
+                side = 1;
             }
         }
         return line;
+    }
+
+    int mapColor(double distance) {
+        // TODO make this function safer (add checks for making sure the value is between something and 255).
+        return (int) Math.ceil(-0.400 * distance + 255);
     }
 
 
