a13281cade25f25ca6b868116acf82fed23c68a2
Ricardo Oliveira <rfo20112004@gmail.com>
2023-11-26 23:32:11 +0000

    basic controller testing

--- src/test/java/org/wolfenstein/model/MenuControllerTest.java
+++ src/test/java/org/wolfenstein/model/MenuControllerTest.java
@@ -1,53 +1,47 @@
 package org.wolfenstein.model;
 
 import net.jqwik.api.*;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.wolfenstein.GUI.GUI;
 import org.wolfenstein.Game;
 import org.wolfenstein.controller.MenuController;
+import org.wolfenstein.state.GameState;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
 public class MenuControllerTest {
-    Menu mockMenu = Mockito.mock(Menu.class);
-    Game mockGame = Mockito.mock(Game.class);
-    MenuController testCtrl = new MenuController(mockMenu);
-    @Provide
-    Arbitrary<GUI.GUIAction> actions() {
-        return Arbitraries.of(GUI.GUIAction.FRONT, GUI.GUIAction.BACK, GUI.GUIAction.SELECT);
+    Menu mockMenu = mock(Menu.class);
+    Game mockGame = mock(Game.class);
+    MenuController mockController = new MenuController(mockMenu);
+
+    @Test
+    public void exitTest() {
+        when(mockMenu.isSelectedExit()).thenReturn(true);
+        mockController.step(mockGame, GUI.GUIAction.SELECT, 0);
+        verify(mockGame, times(1)).setState(null);
     }
-    @Property
-    void stepTest(@ForAll List<GUI.@From("actions") GUIAction> act) {
-        for(GUI.GUIAction a : act) {
-            switch (a) {
-                case FRONT:
-                    Mockito.when(mockMenu.isSelectedExit()).thenReturn(false);
-                    Mockito.when(mockMenu.isSelectedStart()).thenReturn(false);
-                    Mockito.doNothing().when(mockMenu).previousEntry();
-                    break;
-                case BACK:
-                    Mockito.when(mockMenu.isSelectedExit()).thenReturn(false);
-                    Mockito.when(mockMenu.isSelectedStart()).thenReturn(false);
-                    Mockito.doNothing().when(mockMenu).nextEntry();
-                    break;
-                case SELECT:
-                    Mockito.when(mockMenu.isSelectedExit()).thenReturn(false);
-                    Mockito.when(mockMenu.isSelectedStart()).thenReturn(false);
-                    break;
-            }
-            testCtrl.step(mockGame, a, 1);
-            switch (a) {
-                case FRONT:
-                    Mockito.verify(mockMenu, Mockito.times(1)).previousEntry();
-                    break;
-                case BACK:
-                    Mockito.verify(mockMenu, Mockito.times(1)).nextEntry();
-                    break;
-                case SELECT:
-                    Mockito.verify(mockMenu, Mockito.times(1)).isSelectedExit();
-                    Mockito.verify(mockMenu, Mockito.times(1)).isSelectedStart();
-                    break;
-            }
-        }
+    @Test
+    public void enterTest() {
+        when(mockMenu.isSelectedStart()).thenReturn(true);
+        mockController.step(mockGame, GUI.GUIAction.SELECT, 0);
+        verify(mockGame, times(1)).setState(any());
+    }
+    @Test
+    public void moveTest() {
+        Menu menu = new Menu();
+        MenuController menuController = new MenuController(menu);
+        boolean selectedStart = menu.isSelectedStart();
+        assertTrue(selectedStart);
+        menuController.step(mockGame, GUI.GUIAction.BACK, 0);
+        boolean selectedExit = menu.isSelectedExit();
+        assertTrue(selectedExit);
+        menuController.step(mockGame, GUI.GUIAction.FRONT, 0);
+        selectedStart = menu.isSelectedStart();
+        assertTrue(selectedStart);
     }
 }
