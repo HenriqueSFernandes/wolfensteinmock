e4937eeab4c5a819947c82a27127d88f4dd0a766
Ricardo Oliveira <rfo20112004@gmail.com>
2023-12-05 14:38:24 +0000

    implemented guard routine

--- src/main/java/org/wolfenstein/controller/GuardController.java
+++ src/main/java/org/wolfenstein/controller/GuardController.java
@@ -9,35 +9,29 @@ import org.wolfenstein.model.elements.Guard;
 import java.util.Random;
 
 public class GuardController extends GameController {
+
+    int step_counter = 1;
     public GuardController(Camera model) {
         super(model);
     }
-    public void moveForward(Guard guard) { moveGuard(guard.getPosition().moveForward(), guard); }
-    public void rotateClockwise(Guard guard) { moveGuard(guard.getPosition().rotateClockwise(), guard); }
-    public void rotateAntiClockwise(Guard guard) { moveGuard(guard.getPosition().rotateAntiClockwise(), guard); }
-    public void moveBackward(Guard guard) { moveGuard(guard.getPosition().moveBackwards(), guard); }
-    public void moveGuard(Position position, Guard guard) {
+    private void moveForward(Guard guard) { moveGuard(guard.getPosition().moveForward(), guard); }
+    private void rotateClockwise(Guard guard) { moveGuard(guard.getPosition().rotateClockwise(), guard); }
+    private void rotateAntiClockwise(Guard guard) { moveGuard(guard.getPosition().rotateAntiClockwise(), guard); }
+    private void moveBackward(Guard guard) { moveGuard(guard.getPosition().moveBackwards(), guard); }
+    private void moveGuard(Position position, Guard guard) {
         if (getModel().isEmpty(position)) guard.setPosition(position);
     }
+
+
     @Override
     public void step(Game game, GUI.GUIAction action, long time) {
-        Random rand = new Random();
-        int next;
         for (Guard guard : getModel().getGuardList()) {
-            next = rand.nextInt(4);
-            switch (next) {
-                case 0:
-                    moveForward(guard);
-                    break;
-                case 1:
-                    moveBackward(guard);
-                    break;
-                case 2:
-                    rotateClockwise(guard);
-                    break;
-                case 3:
-                    rotateAntiClockwise(guard);
-                    break;
+            if (step_counter == 0 && guard.getPosition().getAngle() < 180) rotateAntiClockwise(guard);
+            else if (step_counter == 10 && guard.getPosition().getAngle() > 0) rotateClockwise(guard);
+            else {
+                moveForward(guard);
+                if (guard.getPosition().getAngle() == 180) step_counter++;
+                else step_counter--;
             }
         }
     }
