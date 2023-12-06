02c78077b8e95eb47b76a6273ed50ec1bc685d3f
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2023-12-06 15:34:30 +0000

    Added Sound and SoundLoader classes
    Also added gun shot sound effect

---
+++ src/main/java/org/wolfenstein/model/sound/SoundLoader.java
@@ -0,0 +1,32 @@
+package org.wolfenstein.model.sound;
+
+import javax.sound.sampled.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SoundLoader {
+    private static SoundLoader instance;
+    List<Sound> sounds = new ArrayList<>();
+
+    private SoundLoader() {
+    }
+
+    public static SoundLoader getInstance() {
+        if (instance == null) {
+            instance = new SoundLoader();
+        }
+        return instance;
+    }
+
+    public void importSound(String soundName){
+        sounds.add(new Sound(soundName));
+    }
+    public void playAllSounds(){
+        for (Sound sound : sounds){
+            sound.play();
+        }
+    }
+    public Sound getSound(int index){
+        return sounds.get(index);
+    }
+}
