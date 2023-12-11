5868d0671e2ece2a434bcd023c6ec67784a17353
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2023-12-11 19:13:51 +0000

    Fixed the issue

--- src/main/java/org/wolfenstein/model/elements/Player.java
+++ src/main/java/org/wolfenstein/model/elements/Player.java
@@ -18,35 +18,48 @@ public class Player extends Element {
         maxHealth = health;
         maxAmmo = ammo;
     }
+
     public static Player getInstance() {
         if (player == null) player = new Player(10, 10, 0);
         return player;
     }
-    //public void setHealth(int h) { health = h; }
-    public int getMaxHealth() {
-        return maxHealth;
-    }
-    public void setMaxHealth(int m) {
-        maxHealth = Math.max(1, m);
-        health = Math.min(health, maxHealth);
-    }
-    public void changeHealth(int d) {
-        health = Math.max(0, Math.min(health + d, maxHealth));
-    }
-    public int getHealth() {
-        return health;
-    }
-    public int getAmmo() {
-        return ammo;
-    }
+
     //public void setAmmo(int a) { ammo = a; }
     public static int getMaxAmmo() {
         return maxAmmo;
     }
+
     public void setMaxAmmo(int m) {
         maxAmmo = Math.max(1, m);
         ammo = Math.min(ammo, maxAmmo);
     }
+
+    //public void setHealth(int h) { health = h; }
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public void setMaxHealth(int m) {
+        maxHealth = Math.max(1, m);
+        health = Math.min(health, maxHealth);
+    }
+
+    public void changeHealth(int d) {
+        health = Math.max(0, Math.min(health + d, maxHealth));
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        for (int i = 0; i < maxHealth; i++) {
+            imageLoader.getImage(i).setActive(i < health);
+        }
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAmmo() {
+        return ammo;
+    }
+
     public void changeAmmo(int d) {
         ammo = Math.max(0, Math.min(ammo + d, maxAmmo));
     }
