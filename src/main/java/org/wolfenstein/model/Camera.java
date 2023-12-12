b4a202462c414aa89094300406612ccd938f7316
Ricardo Oliveira <rfo20112004@gmail.com>
2023-12-12 18:06:20 +0000

    finally removed all comments

--- src/main/java/org/wolfenstein/model/Camera.java
+++ src/main/java/org/wolfenstein/model/Camera.java
@@ -35,7 +35,6 @@ public class Camera {
         return camera;
     }
     public static Camera createCamera(Map map, Player player) {
-        // This function is only used for testing purposes
         if (camera == null) camera = new Camera(map, player);
         return camera;
     }
