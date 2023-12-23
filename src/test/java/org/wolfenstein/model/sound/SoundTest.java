1634e91950ff3dfd07d30357e8d3e7e62be207ce
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2023-12-23 20:04:02 +0000

    100% coverage in sound testing

--- src/test/java/org/wolfenstein/model/sound/SoundTest.java
+++ src/test/java/org/wolfenstein/model/sound/SoundTest.java
@@ -1,5 +1,24 @@
 package org.wolfenstein.model.sound;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.*;
+
 public class SoundTest {
-    // TODO
+    @Test
+    void play() {
+        try {
+            Sound sound = new Sound(getClass().getResource("/sounds/gun_shot.wav"));
+            sound.play();
+        } catch (Exception e) {
+            fail();
+        }
+        Sound sound = new Sound(getClass().getResource("/images/aim.png"));
+        try {
+            sound.play();
+        } catch (RuntimeException e){
+            return;
+        }
+    }
 }
