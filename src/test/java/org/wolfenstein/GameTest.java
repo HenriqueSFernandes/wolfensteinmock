12365de7595c539e36fe46eff472a2e9eb0716dc
Ricardo Oliveira <rfo20112004@gmail.com>
2023-12-23 21:24:21 +0000

    game test

---
+++ src/test/java/org/wolfenstein/GameTest.java
@@ -0,0 +1,15 @@
+package org.wolfenstein;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+
+public class GameTest {
+    @Test
+    public void gameTest() throws IOException, URISyntaxException, FontFormatException {
+        Game.main(null);
+    }
+}
